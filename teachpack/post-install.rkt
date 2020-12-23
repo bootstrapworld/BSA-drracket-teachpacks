ee96553a949fd18bfe4921b5bc49869193e7f101
Matthias Felleisen <matthias@ccs.neu.edu>
2020-12-23 10:36:11 -0500

    improve directory management

--- teachpack/post-install.rkt
+++ teachpack/post-install.rkt
@@ -2,6 +2,8 @@
 
 (provide post-installer)
 
+(require racket/file)
+
 ;; accept one or two or three or four arguments 
 (define (post-installer collects src . x)
   (define STUFF "bootstrap-files")
@@ -17,6 +19,8 @@
                       (lambda (xn)
                         (displayln `[creating ,STUFF in ,home failed])
                         (displayln (exn-message xn)))])
+       (when (directory-exists? to)
+	 (delete-directory/files to))
        (rename-file-or-directory from to #true))]
     [else
      (displayln `[cannot write to ,home])]))
